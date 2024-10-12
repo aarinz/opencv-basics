7049e24efbc773a3a1c1d681f498edd7f89c59b1
Aarin Zacharia <aarinzacharia560@gmail.com>
2024-10-12 12:58:22 +0530

    face recognition

---
+++ Faces/face_train.py
@@ -0,0 +1,39 @@
+import os
+import cv2 as cv
+import numpy as np
+
+people = ['Ben Afflek', 'Elton John', 'Jerry Seinfield', 'Madonna', 'Mindy Kaling']
+DIR = r'/home/aarin-zach/Desktop/Codes/opencv/Assets/faces_imgs/train'
+haar_cascade = cv.CascadeClassifier('/home/aarin-zach/Desktop/Codes/opencv/haar_face.xml')
+
+features = []
+labels = []
+
+def train():
+    for person in people:
+        path = os.path.join(DIR, person)
+        label = people.index(person)
+
+        for img in os.listdir(path):
+            img_path = os.path.join(path, img)
+
+            img_array = cv.imread(img_path)
+            gray = cv.cvtColor(img_array, cv.COLOR_BGR2GRAY)
+
+            faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=10)
+
+            for (x,y,w,h) in faces_rect:
+                faces_roi = gray[y:y+h, x:x+w]
+                features.append(faces_roi)
+                labels.append(label)
+
+train()
+features = np.array(features, dtype='object')
+labels = np.array(labels)
+
+face_recognizer = cv.face.LBPHFaceRecognizer_create()
+face_recognizer.train(features,labels)
+
+face_recognizer.save('face_trained.yml')
+np.save('features.npy', features)
+np.save('labels.npy', labels)
